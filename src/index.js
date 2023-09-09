241c18b5c3e6d80241ced43eb852c871a12f7da9
Katysha94 <Vasilevakata76@gmail.com>
2023-09-09 14:07:07 +0300

    commit-9

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ function breedSelection(evt) {
         const { url, breeds } = data[0];
             const markupCatDesc=
       `<div class="cat-info">
-        <img class="cat-images" src="${url} alt="${breeds[0].name}" width="320"/></div>
+        <img class="cat-images" src="${url}" alt="${breeds[0].name}" width="320"/></div>
         <div class="cat-info-container">
         <h2 class="cat-name">${breeds[0].name}</h2>
         <p class="cat-desc">${breeds[0].description}</p>
